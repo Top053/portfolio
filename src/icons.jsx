f92dbad41b2e63c6c9d51a91fa51edad0180b70d
Lenox Boyd <reallenoxboyd@gmail.com>
2026-02-20 22:03:26 +0300

    add github icon

--- src/icons.jsx
+++ src/icons.jsx
@@ -1,12 +1,12 @@
-import { FaTwitter, FaFacebook, FaInstagram } from 'react-icons/fa';
+import { FaTwitter, FaFacebook, FaGithub } from 'react-icons/fa';
 
 function Contacticons(){
     return(
         <div>
             <div className="icons">
+                <a href="https://github.com/Top053" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700"><FaGithub size={24} /></a>
                 <a href="https://x.com/boyd_dgaf" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700"><FaTwitter size={24} /></a>
                 <a href="https://www.facebook.com/lenox.boyd.12" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700"><FaFacebook size={24} /></a>
-                <a href="https://www.instagram.com/persona.non.grrrata/" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700"><FaInstagram size={24} /></a>
             </div>
         </div>
     );
