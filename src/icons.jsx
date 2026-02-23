86f979bdb1dbc99343282ea7bd9e8b74f45f8725
Lenox Boyd <reallenoxboyd@gmail.com>
2026-02-23 18:05:51 +0300

    remove tailwind classname from icons

--- src/icons.jsx
+++ src/icons.jsx
@@ -4,9 +4,9 @@ function Contacticons(){
     return(
         <div>
             <div className="icons">
-                <a href="https://github.com/Top053" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700"><FaGithub size={24} /></a>
-                <a href="https://x.com/boyd_dgaf" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700"><FaTwitter size={24} /></a>
-                <a href="https://www.facebook.com/lenox.boyd.12" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700"><FaFacebook size={24} /></a>
+                <a href="https://github.com/Top053" target="_blank" rel="noopener noreferrer"><FaGithub size={24} /></a>
+                <a href="https://x.com/boyd_dgaf" target="_blank" rel="noopener noreferrer"><FaTwitter size={24} /></a>
+                <a href="https://www.facebook.com/lenox.boyd.12" target="_blank" rel="noopener noreferrer"><FaFacebook size={24} /></a>
             </div>
         </div>
     );
