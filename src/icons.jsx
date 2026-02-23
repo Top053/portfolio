7c0aaee3118c25c42e826f19374e121825311431
Lenox Boyd <reallenoxboyd@gmail.com>
2026-02-23 18:16:45 +0300

    arranged icons elements

--- src/icons.jsx
+++ src/icons.jsx
@@ -4,9 +4,27 @@ function Contacticons(){
     return(
         <div>
             <div className="icons">
-                <a href="https://github.com/Top053" target="_blank" rel="noopener noreferrer"><FaGithub size={24} /></a>
-                <a href="https://x.com/boyd_dgaf" target="_blank" rel="noopener noreferrer"><FaTwitter size={24} /></a>
-                <a href="https://www.facebook.com/lenox.boyd.12" target="_blank" rel="noopener noreferrer"><FaFacebook size={24} /></a>
+                <a 
+                    href="https://github.com/Top053" 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                >
+                    <FaGithub size={24} />
+                </a>
+                <a 
+                    href="https://x.com/boyd_dgaf" 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                >
+                    <FaTwitter size={24} />
+                </a>
+                <a 
+                    href="https://www.facebook.com/lenox.boyd.12" 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                >
+                    <FaFacebook size={24} />
+                </a>
             </div>
         </div>
     );
