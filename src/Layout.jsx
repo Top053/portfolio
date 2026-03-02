1bc8e895fe3492371a1f9ab21b61af43a7185cf7
Lenox Boyd <reallenoxboyd@gmail.com>
2026-03-02 16:07:32 +0300

    add usestate

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,8 @@
+import { useState } from 'react';
 import { Outlet, NavLink } from 'react-router-dom';
 import Contacticons from "./icons";
 function Layout(){
+    const [menuOpen, setMenuOpen]=useState(false)
     return(
         <div className='layout'>
             <header>
