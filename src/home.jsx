7c5fec8bf25b002e7ef68448744f2511a07193d5
Lenox Boyd <reallenoxboyd@gmail.com>
2026-03-04 20:50:44 +0300

    ally(ui):add className to contact me and view projects button

--- src/home.jsx
+++ src/home.jsx
@@ -9,8 +9,8 @@ function Home(){
                     <p>BOYD<br/><span><b>LENOX</b></span></p>
                     <p id="work">Frontend Software Engineer</p>
                     <div className="btn">
-                        <button type="button">Contact Me</button>
-                        <button type="button" onClick={()=>navigate("/projects")} >View Projects</button>
+                        <button type="button" className="btn-home">Contact Me</button>
+                        <button type="button" onClick={()=>navigate("/projects")} className="btn-home" >View Projects</button>
                     </div>
                 </div>
             </div>
