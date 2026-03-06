5fa03b73771a187806ddedc7a373979517124e22
Lenox Boyd <reallenoxboyd@gmail.com>
2026-03-06 16:17:11 +0300

    refactor(css):replace id selector with class selector

--- src/home.jsx
+++ src/home.jsx
@@ -7,7 +7,7 @@ function Home(){
             <div className="main">
                 <div className="intro">
                     <p>BOYD<br/><span><b>LENOX</b></span></p>
-                    <p id="work">Frontend Software Engineer</p>
+                    <p className="profession">Frontend Software Engineer</p>
                     <div className="btn">
                         <button type="button" className="btn-home">Contact Me</button>
                         <button type="button" onClick={()=>navigate("/projects")} className="btn-home" >View Projects</button>
