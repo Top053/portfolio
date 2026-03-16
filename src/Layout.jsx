c67fce2ab97eb301fff3b8f72f8e5bfce4789ac8
Lenox Boyd <reallenoxboyd@gmail.com>
2026-03-16 16:27:57 +0300

    refactor(navbar):remove unused on click properties

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -13,12 +13,12 @@ function Layout(){
                         {/*Desktop nav links*/}
                         <ul>
                             <li>
-                                <NavLink to="/" end className={({isActive}) => isActive ? "active" : ""} onClick={closeDrawer}>
+                                <NavLink to="/" end className={({isActive}) => isActive ? "active" : ""}>
                                     Home
                                 </NavLink>
                             </li>
                             <li>
-                                <NavLink to="/about" className={({isActive}) => isActive ? "active" : ""} onClick={closeDrawer}>
+                                <NavLink to="/about" className={({isActive}) => isActive ? "active" : ""}>
                                     About
                                 </NavLink>
                             </li>
@@ -28,7 +28,7 @@ function Layout(){
                                 </NavLink>
                             </li>
                             <li>
-                                <NavLink to="/projects" className={({isActive}) => isActive ? "active" : ""} onClick={closeDrawer}>
+                                <NavLink to="/projects" className={({isActive}) => isActive ? "active" : ""}>
                                     Projects
                                 </NavLink>
                             </li>
