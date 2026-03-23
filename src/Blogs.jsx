c0f21d6f7b583ebb122c8e5e8dbccf54062b9f3e
Lenox Boyd <reallenoxboyd@gmail.com>
2026-03-23 17:56:57 +0300

    feat(about):add classname to h1 about

--- src/Blogs.jsx
+++ src/Blogs.jsx
@@ -3,8 +3,8 @@ function Blog(){
     return(
         <div>
             <div className="main">
-                <h1 className="blogs h1">Blog Posts</h1>
-                <p>Explore my thoughts, step-by-step tutorials, and insights on various topics</p>
+                <h1 className="blog h1">Blog Posts</h1>
+                <p className="blog p">Explore my thoughts, step-by-step tutorials, and insights on various topics</p>
             </div>
         </div>
     )
