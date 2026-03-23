f1317e74e2b75864572ff24f2cac039a262908e8
Lenox Boyd <reallenoxboyd@gmail.com>
2026-03-23 18:16:48 +0300

    feat(blogs):add posts data for navbar and css selector articles

--- src/Blogs.jsx
+++ src/Blogs.jsx
@@ -1,4 +1,7 @@
 function Blog(){
+    const posts = [
+        {id:"javascript" , title:"my encounter with javascript"}
+    ]
 
     return(
         <div>
@@ -6,6 +9,12 @@ function Blog(){
                 <h1 className="blog-h1">Blog Posts</h1>
                 <p className="blog-p">Explore my thoughts, step-by-step tutorials, and insights on various topics</p>
             </div>
+
+            {posts.map(post =>(
+                <div key={post.id}>
+                    <h3>{post.title}</h3>
+                </div>
+            ))}
         </div>
     )
 }
