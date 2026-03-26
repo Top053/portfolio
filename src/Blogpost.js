944ebdd37aad225ba19101e6f0193f1e758bf925
Lenox Boyd <reallenoxboyd@gmail.com>
2026-03-26 16:58:05 +0300

    feat(blog):add content

--- src/Blogpost.js
+++ src/Blogpost.js
@@ -1,9 +1,10 @@
 const blogposts = [
         {
-            id:"javascript",
+            id:"my-encounter-with-javascript",
             title:"My Encounter With Javascript",
             summary:"In this article, I provide a little context on my learning journey and my motivations for starting javascript.",
-            date:"March 23, 2026"
+            date:"March 23, 2026",
+            content:"This is my first post"
         }
     ]
     export default blogposts;
