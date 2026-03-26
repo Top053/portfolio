2aeebd20e23a919966d2b7a49c612a1f40f360cd
Lenox Boyd <reallenoxboyd@gmail.com>
2026-03-26 15:28:46 +0300

    feat(blogposts):create blogposts js file

---
+++ src/blogposts.js
@@ -0,0 +1,9 @@
+const posts = [
+        {
+            id:"javascript",
+            title:"My Encounter With Javascript",
+            summary:"In this article, I provide a little context on my learning journey and my motivations for starting javascript.",
+            date:"March 23, 2026"
+        }
+    ]
+    export default posts;
