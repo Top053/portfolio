0faa885a84d9db55bb3af7eccb1881fa079968cc
Lenox Boyd <reallenoxboyd@gmail.com>
2026-03-26 15:53:44 +0300

    refactor(blogs):fix render error by rendering map inside main div

--- src/Blogs.jsx
+++ src/Blogs.jsx
@@ -6,15 +6,15 @@ function Blog(){
             <div className="main">
                 <h1 className="blog-h1">Blog Posts</h1>
                 <p className="blog-p">Explore my thoughts, step-by-step tutorials, and insights on various topics</p>
-            </div>
 
-            {blogposts.map(post =>(
-                <div className="post-container" key={post.id}>
-                    <h3>{post.title}</h3>
-                    <p>{post.summary}</p>
-                    <p>Date Published: {post.date}</p>
-                </div>
-            ))}
+                {blogposts.map(post =>(
+                    <div className="post-container" key={post.id}>
+                        <h3>{post.title}</h3>
+                        <p>{post.summary}</p>
+                        <p>Date Published: {post.date}</p>
+                    </div>
+                ))};
+            </div>
         </div>
     )
 }
